ee08dbfade95c0a4473f116b9160d476ef342630
Vigneswar Mavillapalli <srivigneswar03@gmail.com>
2026-02-22 18:14:37 +0530

    Add files via upload

---
+++ day2_python_basics.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("Next year you will be", age + 1, "years old")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum =", num1 + num2)
+print("Difference =", num1 - num2)
+print("Product =", num1 * num2)
